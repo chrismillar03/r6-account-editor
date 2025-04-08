a16a5c614622a3176fb40603bafa40868299448e
ChrisMillar03 <cjm2003@hotmail.co.uk>
2025-04-08 20:50:29 +0100

    Added compiler scripts

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # r6-account-editor
 Simple tool to change R6 account settings.
 
-## Instructions:
+## Quick start:
 1. Place any Ubisoft account into the `.env` file with the following keys:
 ```
 EMAIL=
 PASSWORD=
 ```
 
-2. Run the application to select an to change and what values.
+2. Run `compile.bat` to create an executable or `run.bat` to run.
+
+3. Start the application to select an account to change and what values.
 
 ## Notes:
-You can use any account in the `.env` it's only used for API calls.
+You can use any account in the `.env` file it's only used for API calls.
 
 Credit to [CNDRD](https://github.com/CNDRD) for creating [siegeapi](https://github.com/CNDRD/siegeapi)
---
+++ compile.py
@@ -0,0 +1,37 @@
+import os, shutil, time
+import PyInstaller.__main__ as pyinstaller
+
+def cleanup(spec_name, delete_dist):
+	spec_file = os.path.join(os.getcwd(), f"{spec_name}.spec")
+	build_dir = os.path.join(os.getcwd(), "build")
+	dist_dir = os.path.join(os.getcwd(), "dist")
+
+	if os.path.exists(spec_file):
+		os.remove(spec_file)
+
+	if os.path.exists(build_dir):
+		shutil.rmtree(build_dir)
+
+	if os.path.exists(dist_dir) and delete_dist:
+		shutil.rmtree(dist_dir)
+
+def main():
+	name = os.getcwd().split("\\")[-1]
+
+	cleanup(name, True)
+
+	pyinstaller.run([
+		f"--name={name}",
+		"--onefile",
+		"--console",
+		"--clean",
+		"--add-data=.env:.",
+		"main.py"
+	])
+
+	cleanup(name, False)
+
+	time.sleep(5)
+
+if __name__ == "__main__":
+	main()
